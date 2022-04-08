e9d5bb469e0e7f451ae92a0df7820c51fad1c4e3
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2022-04-09 08:35:30 +0900

    implement the inverse discrete Fourier transform

---
+++ idft.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def idft(x):
+    n = len(x)
+    e = np.exp(+1j * 2 * np.pi / n)
+    A = [[i * j for j in range(n)] for i in range(n)]
+    return np.dot(e ** A, x) / n ** 0.5
