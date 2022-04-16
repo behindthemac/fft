7410c0976ef5702c12f131d09faf25cfb1008508
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2022-04-17 08:24:53 +0900

    implement the fast Fourier transform

---
+++ fft.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def fft(x):
+    n = len(x)
+    if n <= 1:
+        return x
+
+    X0 = fft(x[0::2])
+    X1 = fft(x[1::2])
+    X = np.concatenate((X0, X1))
+
+    A = np.zeros((n, n), dtype=complex)
+    m = n // 2
+    e = np.exp(-1j * 2 * np.pi / n)
+    for i in range(n):
+        j = i % m
+        A[i][j    ] = 1
+        A[i][j + m] = e ** i
+
+    return np.dot(A, X) / 2 ** 0.5
