c67a56227147273fc6f4ad2e18146fb868df7ea2
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2022-05-27 08:57:17 +0900

    add a docstring to the function idft

--- idft.py
+++ idft.py
@@ -2,6 +2,14 @@ import numpy as np
 
 
 def idft(x):
+    """Get the inverse discrete Fourier transform of a sequence.
+
+    Args:
+    x: A sequence of complex numbers
+
+    Returns:
+    The inverse discrete Fourier transform of the sequence
+    """
     n = len(x)
     e = np.exp(+1j * 2 * np.pi / n)
     A = [[i * j for j in range(n)] for i in range(n)]
