7177aa23cbc9d70743c02592f606be6d65facc13
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2023-09-09 19:15:13 +0900

    implement the inverse fast Fourier transform

---
+++ ifft.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def ifft(x):
+    """Returns the inverse discrete Fourier transform of a sequence.
+
+    Args:
+        x: A sequence of complex numbers
+
+    Returns:
+        The inverse discrete Fourier transform of the sequence
+    """
+    n = len(x)
+    if n <= 1:
+        return x
+
+    X0 = ifft(x[0::2])
+    X1 = ifft(x[1::2])
+    X = np.concatenate((X0, X1))
+
+    A = np.zeros((n, n), dtype=complex)
+    m = n // 2
+    e = np.exp(+1j * 2 * np.pi / n)
+    for i in range(n):
+        j = i % m
+        A[i][j    ] = 1
+        A[i][j + m] = e ** i
+
+    return np.dot(A, X) / 2 ** 0.5
